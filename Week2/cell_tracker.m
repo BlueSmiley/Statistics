1819ecb77c38404c029a73b76d8f47a95e0f8ec1
bluesmiley <BlueSmiley@users.noreply.github.com>
2019-02-15 02:26:46 +0000

    hahah

--- Week2/cell_tracker.m
+++ Week2/cell_tracker.m
@@ -3,17 +3,30 @@ function resGrid = cell_tracker(locGrid, obsGivenLocGrid)
     if [rowLen,colLen] ~= size(obsGivenLocGrid)
         error("grid dimensions are different");
     end
+    %Calculate P(observation)
+    obs = calcObs(locGrid,obsGivenLocGrid);
     resGrid = zeros(rowLen,colLen);
     for i = 1:rowLen
         for j = 1:colLen
-            % probability of observation is taken to be 100%
-            % We assume no probability changes in time until observation
             resGrid(i,j) = calcCondLocProb(locGrid(i,j), ...
-                obsGivenLocGrid(i,j),1);
+                obsGivenLocGrid(i,j),obs);
         end
     end
 end
 
 function res = calcCondLocProb(locProb,obsGivenLocProb,obsProb)
     res = (obsGivenLocProb*locProb)/obsProb;
+end
+
+function res = calcObs(locGrid,obsGivenLocGrid)
+    [rowLen,colLen] = size(locGrid);
+    res = 0;
+    if [rowLen,colLen] ~= size(obsGivenLocGrid)
+        error("grid dimensions are different");
+    end
+    for i = 1:rowLen
+        for j = 1:colLen
+            res = res + locGrid(i,j)*obsGivenLocGrid(i,j);
+        end
+    end
 end
